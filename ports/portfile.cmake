6a8a36c2e78b19f613ca24b850693644b0942611
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-01-10 13:56:29 -0500

    switch ref to main repo

--- ports/portfile.cmake
+++ ports/portfile.cmake
@@ -1,12 +1,11 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO nikitalita/cppdap
-  REF 952d6e2f991812291c8eb2b5ae0707505110dc31
-  SHA512 97399187f5457c3e811c77b295340fc5cd15baeb1430b860a76583972ac6e12a610ad1c41de804e5f6b20bfa45872b7c9b439d323b81d1f75a409a8dd1b9db65
-  HEAD_REF master
+  REPO google/cppdap
+  REF 0a340c6d71ca00893ca1aefea38f3504e6755196
+  SHA512 14f8d0438678eb715f171b95ed9733485bdb681bc174642f196b7665aad3157b500ba6e6abccc5adcb8ec17aeffa94ae562dcbd31248244d9c8f4a11b97fc2ea
+  HEAD_REF main
 )
-# Check if one or more features are a part of a package installation.
-# See /docs/maintainers/vcpkg_check_features.md for more details
+
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
   FEATURES
     use-nlohmann-json CPPDAP_USE_EXTERNAL_NLOHMANN_JSON_PACKAGE
@@ -15,6 +14,8 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 
 if (NOT CPPDAP_USE_EXTERNAL_NLOHMANN_JSON_PACKAGE AND NOT CPPDAP_USE_EXTERNAL_RAPIDJSON_PACKAGE)
   message(FATAL_ERROR "Must set either \"use-nlohmann-json\" or \"use-rapidjson\" feature.")
+elseif(CPPDAP_USE_EXTERNAL_NLOHMANN_JSON_PACKAGE AND CPPDAP_USE_EXTERNAL_RAPIDJSON_PACKAGE)
+  message(FATAL_ERROR "Cannot set both \"use-nlohmann-json\" and \"use-rapidjson\" feature.")
 endif()
 
 vcpkg_cmake_configure(
